b585115e63b378eac3d5645c80e0a0915f11b2bc
mayurPatil45 <patilmayur.2745@gmail.com>
2024-06-10 09:58:02 +0530

    Backend Frontend Connection

--- server/index.js
+++ server/index.js
@@ -9,10 +9,14 @@ app.use(cors());
 
 require('dotenv').config();
 
-app.use(cors());
+app.use(cors({
+    origin: ["https://medical-pager-frontend.vercel.app/"],
+    methods: ["POST", "GET"],
+    credentials: True,
+}));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://real-time-chat-application-frontend.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', 'https://medical-pager-frontend.vercel.app/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
